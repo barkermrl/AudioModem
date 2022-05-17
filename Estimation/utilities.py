106ddbda4fd669eab5fe1a1cfd99cc7fc14bcffe
moonear2000 <mounir.hinedi@gmail.com>
2022-05-17 16:10:03 +0100

    updated utilities

--- Estimation/utilities.py
+++ Estimation/utilities.py
@@ -4,14 +4,14 @@ from scipy.signal import chirp
 import sounddevice as sd
 import matplotlib.pyplot as plt
 
-def gen_chirp(f0, f1, T, save = False, play = True, form = 'logarithmic', repititions = 1):
+def gen_chirp(T, save = False, play = True, form = 'logarithmic', repititions = 1):
     fs = 44100
     t = np.arange(0, int(T*fs))/fs
-    w = chirp(t, f0, T, f1, method = form)
+    w = chirp(t, 20, T, 20000, method = form)
     if repititions > 1:
         w = np.tile(w, repititions)
     if save == True:
-        write('chirp_{}_{}_{}.wav'.format(form, f0, f1), fs, w)
+        write('chirp_{}_{}_{}.wav'.format(form, 20, 20000), fs, w)
     if play == True:
         sd.play(w, fs, blocking = True)
     return w
@@ -20,7 +20,7 @@ def record(title = 'test', duration = 5, save = False, plot = False):
     fs = 44100
     r = sd.rec(int(duration*fs), samplerate = fs, channels = 1, blocking = True).flatten()
     if save  == True:
-        write(title, fs, r)
+        write("{}.wav".format(title), fs, r)
     if plot == True:
         plt.plot(np.arange(0, int(duration*fs)), r)
         plt.show()
@@ -30,7 +30,7 @@ def conv(r, w):
     fs = 44100
     T = len(w)/fs
     w_inv = np.flip(w)
-    convolution = np.conv(r,w_inv)
+    convolution = np.convolve(r,w_inv)
     t = np.arange(0, int(T*fs))/fs
-    plt.plot(t, convolution)
+    plt.plot(convolution)
     plt.show()
