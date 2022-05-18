b808ec55300e932a783e7cc533b49c648740c1ea
moonear2000 <mounir.hinedi@gmail.com>
2022-05-18 17:41:43 +0100

    updated utilities.py and added new notebook used to test sychronisation of white noise blocks using a chirp

--- Estimation/utilities.py
+++ Estimation/utilities.py
@@ -1,10 +1,10 @@
 import numpy as np
-from scipy.io.wavfile import write
+from scipy.io.wavfile import write, read
 from scipy.signal import chirp 
 import sounddevice as sd
 import matplotlib.pyplot as plt
 
-def gen_chirp(T, save = False, play = True, form = 'logarithmic', repititions = 1):
+def gen_chirp(T, save = False, play = False, form = 'logarithmic', repititions = 1):
     fs = 44100
     t = np.arange(0, int(T*fs))/fs
     w = chirp(t, 20, T, 20000, method = form)
@@ -28,9 +28,13 @@ def record(title = 'test', duration = 5, save = False, plot = False):
 
 def conv(r, w):
     fs = 44100
-    T = len(w)/fs
     w_inv = np.flip(w)
     convolution = np.convolve(r,w_inv)
-    t = np.arange(0, int(T*fs))/fs
-    plt.plot(convolution)
+    t = np.arange(0, len(convolution))/fs
+    plt.plot(t, np.abs(convolution))
     plt.show()
+    
+def read_wav(filename):
+    fs, data = read(filename)
+    return data
+
