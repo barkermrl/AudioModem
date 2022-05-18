a721bc53f31ae56e9a9351e503cf2b9c3847111a
finngoodall <finn.goodall@gmail.com>
2022-05-18 18:31:58 +0100

    Changed to recording the tests each time instead of getting them out of files

--- Estimation/channel_estimation_test.py
+++ Estimation/channel_estimation_test.py
@@ -41,9 +41,12 @@ def samples_from_wav(filename):
 
 # Finds the start index of the received chirp by convolving with the time-reversed known chirp
 # Returns the start index of the chirp in the unknown signal array
-def get_chirp_start_index(known_signal, unknown_signal):
-	convolution = np.convolve(y_test, np.flip(x_test))
+def get_chirp_start_index(sent_chirp, received_chirp):
+	convolution = np.convolve(received_chirp, np.flip(sent_chirp))
 	signal_start_index = np.argmax(abs(convolution))
+
+	plt.plot(convolution)
+	plt.show()
 
 	return signal_start_index
 
@@ -55,7 +58,9 @@ def split_sample(data, section_length):
 	num_sections = 1 + data.size//section_length
 
 	# Pad data with zeroes so it is a multiple of the section length
-	padded_data = np.pad(data, num_sections*section_length - data.size)
+	padded_data = np.zeros(num_sections*section_length)
+	padded_data[:data.size] = data
+	
 	split_data = np.split(padded_data, num_sections)
 	
 	for i in range(num_sections):
@@ -66,106 +71,58 @@ def split_sample(data, section_length):
 
 
 # Transmitted and received test chirps for channel estimation
-transmitted_estimator_filename = 'channel_test_transmitted_int16.wav'
-received_estimator_filename = 'channel_test_received_int16.wav'
+fs = 44100
+chirp_f_start = 0
+chirp_f_end = 20000
+duration = 2
+time_samples = np.arange(0, int(duration), 1/fs)
 
-x_est = samples_from_wav(transmitted_estimator_filename)
-y_est = samples_from_wav(received_estimator_filename)
+test_chirp = chirp(time_samples, chirp_f_start, duration, chirp_f_end, method = 'linear')
+print('RECORDING')
+received_chirp = sd.rec(int(2.5*duration*fs), samplerate = fs, channels = 1, blocking = True).flatten()
+print('RECORDING FINISHED')
 
 # Synchronise to find the start of the chirp in the received sample
-signal_start_index = get_chirp_start_index(x_test, y_test)
+signal_start_index = get_chirp_start_index(test_chirp, received_chirp)
 section_length = 1024
-num = 100
-section_start_indicies = signal_start_index + np.linspace(0, section_length*num, num+1)
+num_sections = 1 + test_chirp.size//section_length
+
+section_start_indicies = signal_start_index + np.linspace(0, section_length*num_sections, num_sections+1)
 
 # Split the estimation chirps into bins by time section
-x_est_sections = split_sample(x_est, section_length)
-y_est_sections = split_sample(y_est[signal_start_index:], section_length)		# Truncation ensures only the received data from the chirp onwards is kept
+test_chirp_sections = split_sample(test_chirp, section_length)
+received_chirp_sections = split_sample(received_chirp[signal_start_index:], section_length)		# Truncation ensures only the received data from the chirp onwards is kept
 
-assert len(y_est_sections.keys()) >= len(x_est_sections.keys())		# Test to ensure the received chirp has more sections than the transmitted chirp i.e. from the recording after the test chirp stops
+assert len(received_chirp_sections.keys()) >= len(test_chirp_sections.keys())		# Test to ensure the received chirp has more sections than the transmitted chirp i.e. from the recording after the test chirp stops
 
 # Plot the received signal and the indicies of the section it's divided into
-plt.plot(np.abs(y_test), color = 'blue')
-plt.vlines(section_start_indicies, color = 'red')
+plt.plot(received_chirp, color = 'blue')
+plt.vlines(section_start_indicies, ymin = 0, ymax = 1, color = 'red')
 plt.show()
 
 
 # Find the DFT of each section
 X_est_sections = {}
-for section_number in x_est_sections.keys():
-	X_est_sections[section_number] = np.fft.fft[x_est_sections[section_number]]/np.sqrt(section_length)
+for section_number in test_chirp_sections.keys():
+	X_est_sections[section_number] = np.fft.fft(test_chirp_sections[section_number])/np.sqrt(section_length)
 
 Y_est_sections = {}
-for section_number in y_est_sections.keys():
-	Y_est_sections[section_number] = np.fft.fft[y_est_sections[section_number]]/np.sqrt(section_length)
+for section_number in received_chirp_sections.keys():
+	Y_est_sections[section_number] = np.fft.fft(received_chirp_sections[section_number])/np.sqrt(section_length)
 
 # Find the corresponding start frequency of the section
 # Currently using a linear 0Hz-20kHz chirp to estimate the channel so the sample index (time) is proportional to the freqeuncy
-chirp_start_freq = 0
-chirp_end_freq = 20000
-num_bins = 1 + x_est.size//section_length
-
-bin_freq_range = (chirp_end_freq-chirp_start_freq)/num_bins
+bin_freq_range = (chirp_f_end-chirp_f_start)/num_sections
 
 # Find the channel FFT in each frequency bin by dividing corresponding sections in X and Y
 H_estimated_sections = {}
-for section_number in x_est_sections.keys():
+for section_number in test_chirp_sections.keys():
 	X = X_est_sections[section_number]
 	Y = Y_est_sections[section_number]
 
-	bin_start_freq = chirp_start_freq + section_number*bin_freq_range
+	bin_start_freq = chirp_f_start + section_number*bin_freq_range
 
 	try:
 		H_estimated_sections[bin_start_freq] = Y/X
 	except:			# In case one of the sections in X is full of zeroes, giving a divide by zero error
 		H_estimated_sections[bin_start_freq] = np.zeros(section_length)
-
-
-full_H = np.concatenate((H_estimated_sections.values()))
-plt.plot(np.abs(full_H))
-plt.vlines(H_estimated_sections.keys())
-
-"""
-print('x length = {}\ny length = {}\nconv length = {}\nsignal start index = {}'.format(x.size, y.size, convolution.size, signal_start_index))
-plt.plot(convolution)
-plt.show()
-
-
-
-# Calculate FFTs of the received and transmitted chirps. The received signal y is truncated to the start of the chirp being detected at the peak of the convolution
-y_trunc = y[signal_start_index:]
-Y = np.fft.fft(y_trunc)
-Y /= np.sqrt(Y.size)
-
-X = np.fft.fft(x, n=y_trunc.size)
-X /= np.sqrt(X.size)
-
-
-# Least-Squares channel frequency estimate
-H_ls = X/Y
-
-fs = 44100
-fft_length = H_ls.size
-freq_samples = np.linspace(0, fs, fft_length)
-H_ls_max = np.amax(np.abs(H_ls))
-
-# Dict contains key, value pairs as follows:
-# Key =  is the noise noise cut-off point i.e. noisy frequency components are decided to be that percentage of the max absolute value of H_ls
-# Value is the resulting array, removing all noisy components form H_ls
-H_ls_nonoise_dict = {
-	0.2 : [],
-	0.1 : [],
-	0.05 : [],
-	0.01 : [],
-}
-
-for cutoff in H_ls_nonoise_dict.keys():
-	mask = np.abs(H_ls) > cutoff * H_ls_max
-	H_ls_nonoise_dict[cutoff] = H_ls[mask]
-
-
-# Plot the channel frequency spectrum for positive frequencies; the channel is real so the DFT is symmetric
-# Assume all frequency components with values < 0.1*max{H_ls} are negligible
-plt.plot(freq_samples, np.abs(H_ls_nonoise_dict[0.1]))
-plt.show()
-"""
