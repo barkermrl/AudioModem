1d8ead388de30e0810b1c03c6fd934cac727875d
Matthew <34818927+barkermrl@users.noreply.github.com>
2022-06-04 22:15:44 +0100

    Added constants, started refactoring

--- transmission.py
+++ transmission.py
@@ -6,107 +6,105 @@ import scipy.io.wavfile as wav
 import sounddevice as sd
 import subprocess
 
+# Only use frequency bins 86-853 inclusive
+FREQ_MIN = 86
+FREQ_MAX = 854
 
-class Chirp:
-    def __init__(self, fmin=1e3, fmax=10e3, duration=1, fs=48000):
-        self.fmin = fmin
-        self.fmax = fmax
-        self.duration = duration
-        self.fs = fs
+N_BINS = FREQ_MAX - FREQ_MIN
 
-        # Create chirp signal using scipy chirp function.
-        t = np.linspace(0, self.duration, self.fs)
-        self.signal = sig.chirp(t, fmin, duration, fmax, method="linear")
+
+CONSTELLATION_MAP = {
+    "00": complex(1 / np.sqrt(2), 1 / np.sqrt(2)),
+    "01": complex(-1 / np.sqrt(2), 1 / np.sqrt(2)),
+    "11": complex(-1 / np.sqrt(2), -1 / np.sqrt(2)),
+    "10": complex(1 / np.sqrt(2), -1 / np.sqrt(2)),
+}
+
+KEYS = list(CONSTELLATION_MAP.keys())
+VALUES = list(CONSTELLATION_MAP.values())
+
+
+FS = 48000
+L = 512
+N = 4096
+
+NUM_CARRIERS = (N - 2) // 2
+KNOWN_SYMBOL = np.load("known_ofdm_symbol.npy")
+GUARD = KNOWN_SYMBOL[-L:]
+
+
+t = np.linspace(0, 1, FS)
+CHIRP = sig.chirp(t, 1e3, 1, 10e3, method="linear")
+PREAMBLE = np.concatenate(
+    [CHIRP, GUARD, KNOWN_SYMBOL, KNOWN_SYMBOL, KNOWN_SYMBOL, KNOWN_SYMBOL]
+)
+ENDAMBLE = np.concatenate(
+    [GUARD, KNOWN_SYMBOL, KNOWN_SYMBOL, KNOWN_SYMBOL, KNOWN_SYMBOL, CHIRP]
+)
+
+START = np.concatenate([np.zeros(FS), CHIRP])
+END = np.concatenate([CHIRP, np.zeros(FS)])
 
 
 class Transmission:
-    def __init__(self, source, constellation_map, L=512, N=4096, fs=48000):
+    def __init__(self, source):
         self.source = source
-        self.L = L
-        self.N = N
-        self.fs = fs
-        self.constellation_map = constellation_map
-
-        # Calculate number of carriers used to transmit information
-        self.num_data_carriers = (self.N - 2) // 2
+        self.source_chunks = source.reshape((-1, N_BINS))
 
         # Create signal
         symbols = self._create_symbols()
-        header = self._create_header()
-        body = self._create_body(symbols)
-        footer = self._create_footer()
-        self.signal = np.concatenate((header, body, footer))
+        frame = np.concatenate([PREAMBLE, symbols, ENDAMBLE])
+
+        # TODO: Add file size in header
+        self.signal = np.concatenate((START, frame, END))
 
     def _create_symbols(self):
         symbols = []
         self.Xs = []
 
-        for source_chunk in self._chunk_source():
+        for source_chunk in self.source_chunks:
+            data_carriers = np.concatenate(
+                (
+                    np.random.choice(VALUES, NUM_CARRIERS - FREQ_MIN),
+                    source_chunk,
+                    np.random.choice(VALUES, NUM_CARRIERS - FREQ_MAX),
+                )
+            )
             subcarrier_data = np.concatenate(
-                ([0], source_chunk, [0], np.conjugate(np.flip(source_chunk)))
+                ([0], data_carriers, [0], np.conjugate(np.flip(data_carriers)))
             )
             self.Xs.append(subcarrier_data)
             x = np.fft.ifft(subcarrier_data).real
-            OFDM_symbol = np.concatenate((x[-self.L :], x))
+            OFDM_symbol = np.concatenate((x[-L:], x))
             OFDM_symbol /= np.max(np.abs(OFDM_symbol))
 
             symbols.append(OFDM_symbol)
-        return symbols
-
-    def _chunk_source(self):
-        # Need to pad the source with end zeros if required.
-        # Calculate remainder when length of source is divided by number of
-        # carriers used to transmit information.
-        end_zeros = np.zeros(
-            self.num_data_carriers - (len(self.source) % self.num_data_carriers)
-        )
-        source_with_end_zeros = np.concatenate([self.source, end_zeros])
-
-        # Reshape 1D array into 2D array with shape  (Y x num_data_carriers).
-        shape = (
-            len(source_with_end_zeros) // self.num_data_carriers,
-            self.num_data_carriers,
-        )
-        return source_with_end_zeros.reshape(shape)
-
-    def _create_header(self):
-        pause = np.zeros(self.fs)
-        self.chirp = Chirp(fmin=1e3, fmax=10e3, duration=1, fs=self.fs)
-        return np.concatenate([pause, self.chirp.signal])
-
-    def _create_footer(self):
-        self.chirp = Chirp(fmin=1e3, fmax=10e3, duration=1, fs=self.fs)
-        pause = np.zeros(self.fs)
-        return np.concatenate([self.chirp.signal, pause])
-
-    def _create_body(self, symbols):
-        # TODO Include periodic resynchronisations/estimations etc.
-        # For now only concatenates the symbols into one long signal.
         return np.concatenate(symbols)
 
     def save_signals(self, t_fname="files/signal.wav", r_fname="files/received.wav"):
-        wav.write(r_fname, self.fs, self.received_signal)
-        wav.write(t_fname, self.fs, self.signal)
+        wav.write(r_fname, FS, self.received_signal)
+        wav.write(t_fname, FS, self.signal)
 
     def load_signals(self, t_fname="files/signal.wav", r_fname="files/received.wav"):
         # Read transmitted signal to ensure it matches signal tranmission is
         # expecting.
         fs, signal = wav.read(t_fname)
         np.testing.assert_equal(self.signal, signal)
-        assert self.fs == fs
+        assert fs == FS
 
         fs, self.received_signal = wav.read(r_fname)
+        assert fs == FS
 
     def record_signal(self, afplay=False):
         if afplay:
             # Requires afplay to be installed as a terminal command!
-            wav.write("tmp.wav", self.fs, self.signal)
+            wav.write("tmp.wav", FS, self.signal)
             subprocess.Popen(["afplay", "tmp.wav"])
 
         print("Recording...")
         self.received_signal = sd.rec(
-            int(1.5 * self.fs) + len(self.signal),
-            samplerate=self.fs,
+            int(1.5 * FS) + len(self.signal),
+            samplerate=FS,
             channels=1,
             blocking=True,
         ).flatten()
@@ -116,16 +114,12 @@ class Transmission:
         # End of chirp is half a second after chirp.
         # Offset gives the number of samples to shift back by to ensure you're sampling early.
         peaks = self._find_chirp_peaks()
-        frame_start_index = peaks.min() + self.fs // 2 - offset
-        frame_end_index = peaks.max() - self.fs // 2 - offset
+        frame_start_index = peaks.min() + len(CHIRP) // 2 + len(PREAMBLE) - offset
+        frame_end_index = peaks.max() - len(CHIRP) // 2 + len(PREAMBLE) - offset
 
-        num_symbols = np.round(
-            (frame_end_index - frame_start_index) / (self.L + self.N)
-        )
+        num_symbols = np.round((frame_end_index - frame_start_index) / (L + N))
 
-        sampling_error = (
-            frame_end_index - frame_start_index - num_symbols * (self.L + self.N)
-        )
+        sampling_error = frame_end_index - frame_start_index - num_symbols * (L + N)
 
         print(
             "Peaks: {}\nFrame start: {}\nFrame end: {}\nNum symbols: {}\nSampling error: {}".format(
@@ -153,12 +147,12 @@ class Transmission:
         # window = lambda x: x * np.hanning(len(x))
         conv = sig.convolve(
             self.received_signal,
-            np.flip(self.chirp.signal),
+            np.flip(CHIRP),
             mode="same",
         )
-        peaks = sig.find_peaks(
-            conv, height=0.5 * np.abs(conv).max(), distance=transmission.fs - 1
-        )[0]
+        peaks = sig.find_peaks(conv, height=0.5 * np.abs(conv).max(), distance=FS - 1)[
+            0
+        ]
         return peaks
 
     def _identify_Rs(self, frame_start_index, num_symbols, sampling_error):
@@ -167,11 +161,9 @@ class Transmission:
 
         for i in range(n):
             start = int(
-                frame_start_index
-                + self.L
-                + np.rint((self.L + self.N + error_per_symbols) * i)
+                frame_start_index + L + np.rint((L + N + error_per_symbols) * i)
             )
-            end = start + self.N
+            end = start + N
             r = self.received_signal[start:end]
             R = np.fft.fft(r)
             Rs.append(R)
@@ -194,7 +186,7 @@ class Transmission:
 
             # Only add useful part of carrier data X
             # Bins determined by standard (86 to 854 inclusive)
-            self.Xhats.append(X[86:854])
+            self.Xhats.append(X[FREQ_MIN:FREQ_MAX])
 
     def _unwrap_phases(self):
         # Correct for wrapping of phases to [pi, -pi]
@@ -260,7 +252,7 @@ class Transmission:
     def plot_channel(self):
         _, (ax_left, ax_right) = plt.subplots(1, 2, figsize=(15, 5))
 
-        freqs = np.linspace(0, self.fs, self.H_est.shape[0])
+        freqs = np.linspace(0, FS, self.H_est.shape[0])
 
         ax_left.plot(freqs, 10 * np.log10(np.abs(self.H_est)))
         ax_left.set_xlabel("Frequency [Hz]")
@@ -290,30 +282,21 @@ class Transmission:
 
         plt.show()
 
+    def mse_decode(self, i=-1):
+        Xhat = self.Xhats[i]
+        X = self.source_chunks[i][FREQ_MIN:FREQ_MAX]
+        breakpoint()
 
-fs = 48000
-constellation_map = {
-    "00": complex(1 / np.sqrt(2), 1 / np.sqrt(2)),
-    "01": complex(-1 / np.sqrt(2), 1 / np.sqrt(2)),
-    "11": complex(-1 / np.sqrt(2), -1 / np.sqrt(2)),
-    "10": complex(1 / np.sqrt(2), -1 / np.sqrt(2)),
-}
-#         self.constellation_map = {
-#             0: 1,
-#             1: -1,
-#         }
 
-# Known OFDM symbols from the standard
-known_symbol = np.load("known_ofdm_symbol.npy")[1 : 4096 // 2]
 n = 25
-source = np.tile(known_symbol, n)
+source = np.random.choice(VALUES, N_BINS * n)
 
 np.seterr(all="ignore")  # Supresses runtime warnings
 
-transmission = Transmission(source, constellation_map, fs=fs)
-# transmission.record_signal(afplay=True)
-# transmission.save_signals()
-transmission.load_signals()
+transmission = Transmission(source)
+transmission.record_signal(afplay=True)
+transmission.save_signals()
+# transmission.load_signals()
 
 # Initial synchronisation
 transmission.synchronise(plot=True)
@@ -329,11 +312,12 @@ transmission.sync_correct()
 # transmission.estimate_Xhats()
 transmission.plot_channel()
 # transmission.plot_decoded_symbols()
+transmission.mse_decode()
 
 
 """
 - Adjust OFDM symbol indicies from samples extra/missing (DONE)
-- Change estimation to average magnitudes and angles separately
+- Change estimation to average magnitudes and angles separately (DONE)
 - Add a function to check the error rate between the transmitted and received constellations
     - Use decoder?
 """
